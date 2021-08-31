135c1c4e777d674eb5091257ee7f8f7cec46ab31
chokrihamza <chokrihamza1994@gmail.com>
2021-08-31 14:44:19 +0200

    first call back

--- src/callbacks/TestCallbacks.java
+++ src/callbacks/TestCallbacks.java
@@ -1,5 +1,27 @@
 package callbacks;
 
+interface Callback {
+	void call(String string);
+}
+
 public class TestCallbacks {
 
+	public static void main(String[] args) {
+		Callback myCall;
+		myCall=(element)->{System.out.println("Hello "+element);};
+		sayhello("Jhon", myCall);
+		
+		
+		
+		
+	}
+	
+	public static void  sayhello(String string, Callback function) {
+		
+		function.call(string);
+		
+	};
+	
+	
+	
 }
